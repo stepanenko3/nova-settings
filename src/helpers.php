cac3bda3b6b08af02323ab8bf0331f53c632dd82
Artem Stepanenko <31134245+stepanenko3@users.noreply.github.com>
2024-03-31 22:56:05 +0300

    Optimize cache hits (#16)
    * Optimize cache hits

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
 
 if (!function_exists('settings')) {
     function settings(
@@ -13,16 +14,27 @@ if (!function_exists('settings')) {
             $env = config('app.env');
         }
 
-        $settings = Cache::remember(
-            key: 'settings.' . $section . '.' . $env,
-            ttl: config('cache.lifetime'),
-            callback: fn () => config('nova-settings.model')::query()
-                ->select('settings')
-                ->where('slug', $section)
-                ->where('env', $env)
-                ->first()
-                ->settings ?? [],
-        );
+        $configKey = 'nova-settings.storage.' . $section . '.' . $env;
+
+        if ($value = config($configKey)) {
+            $settings = json_decode($value, true);
+        } else {
+            $settings = Cache::remember(
+                key: 'settings.' . $section . '.' . $env,
+                ttl: config('nova-settings.cache_lifetime', 3600),
+                callback: fn () => config('nova-settings.model')::query()
+                    ->select('settings')
+                    ->where('slug', $section)
+                    ->where('env', $env)
+                    ->first()
+                    ->settings ?? [],
+            );
+
+            Config::set(
+                key: $configKey,
+                value: json_encode($settings),
+            );
+        }
 
         if ($key === null) {
             return $settings;
